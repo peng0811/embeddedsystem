aa3d0e3cfe2ab581dce41ed3e237f353b4e0843f
羅嘉賢 <r08543022@ntu.edu.tw>
2021-06-23 23:34:59 +0800

    0623

---
+++ client.py
@@ -0,0 +1,111 @@
+from bluepy.btle import Peripheral, UUID
+from bluepy.btle import Scanner, DefaultDelegate
+from openpyxl import load_workbook
+import time
+from random import randint
+import asyncio
+import socket
+import json
+import random
+import websockets
+import time
+import threading
+wb=load_workbook('data3.xlsx')
+sheet=wb['Sheet1']
+
+class ScanDelegate(DefaultDelegate):
+	def __init__(self):
+		DefaultDelegate.__init__(self)
+	def handleDiscovery(self, dev, isNewDev, isNewData):
+		if isNewDev:
+			print ("Discovered device", dev.addr)
+		elif isNewData:
+			print ("Received new data from", dev.addr)
+
+scanner = Scanner().withDelegate(ScanDelegate())
+devices = scanner.scan(10.0)
+num=0
+run = True
+print("s")
+async def web_socket(websocket,path):
+    global num
+    global data
+    global times
+    times = 0
+    while(run):
+        #num = randint(360,390)
+        #num = num/10
+        data_rec()
+        times += 1
+        print(times)
+        data= "{\"val\":%d , \"time\":%d}" % (num,times)
+        #data = "{\"time\":%d}" % (times)
+        time.sleep(1)
+        await websocket.send(data)
+    
+
+n=0
+for dev in devices:
+	print ("%d: Device %s (%s), RSSI=%d dB" % (n, dev.addr,dev.addrType, dev.rssi))
+	n = n+1
+	for (adtype, desc, value) in dev.getScanData():
+		print (" %s = %s" % (desc, value))
+
+number = input('Enter your device number: ')
+print('Device', number)
+
+number = int(number)
+n=0
+for dev in devices:
+	print ("%d: Device %s (%s), RSSI=%d dB" % (n, dev.addr,dev.addrType, dev.rssi))
+	if n == number:
+		word = dev.addr
+	n = n+1
+	for (adtype, desc, value) in dev.getScanData():
+		print (" %s = %s" % (desc, value))
+
+idx=2
+print(word)
+print ("Connecting...")
+dev = Peripheral(word, 'random')
+print ("Services...")
+for svc in dev.services:
+    print (str(svc))
+k=1
+try:
+    testService = dev.getServiceByUUID(UUID(0xfff0))
+    for ch in testService.getCharacteristics():
+        print (str(ch))
+        ch = dev.getCharacteristics(uuid=UUID(0xfff1))[0]
+        def data_rec():
+            global num
+            global ID
+            global idx
+            if (ch.supportsRead()):
+                print ("請輸入ID:")
+                ID=input()
+                sheet.cell(row=idx,column=1,value=ID)
+                while 1:
+                    c = ch.read()
+                    time.sleep(2)
+                    if (c[0]<=35 or c[0] >=40)and k:
+                        print ("請靠近點")
+                        time.sleep(1)
+                    if c[0] >= 35:
+                        num =(int(c[0])+256)/10
+                        print(num)
+                        sheet.cell(row=idx,column=2,value=num)
+                        wb.save('data3.xlsx')
+                        idx = idx + 1
+                        num=num*10
+                        time.sleep(0.5)
+                        break
+
+    #t1 = threading.Thread(target=data_rec)
+    #t1.start()
+
+    start_server = websockets.serve(web_socket,"127.0.0.1",5050)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+finally:
+	dev.disconnect()
